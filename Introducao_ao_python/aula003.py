39ad981449dbf250534685bbccb5a820e9c3c750
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-01-23 21:17:11 -0300

    Strings.

---
+++ Introducao_ao_python/aula003.py
@@ -0,0 +1,11 @@
+# Python é uma função dinâmica
+
+# Aspas simples:
+print('Luiz Otávio')
+
+# Aspas duplas:
+print("Luiz Otávio")
+
+#Escape:
+print("Luiz \"Otávio ") # Não recomendado
+print(1, 'Luiz "Otávio"', sep=" -> ")
