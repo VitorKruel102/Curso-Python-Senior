58d0e30920d76b8004713d4d28c252e7ec3ced82
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-01-24 20:55:07 -0300

    Coerção de tipos.

---
+++ Introducao_ao_python/aula006.py
@@ -0,0 +1,9 @@
+# Conversões de tipos
+# Type : int, str, float, bool
+
+print('1', int('1'), type('1'),sep=' -> ')
+print('1', float('1'), type('1'),sep=' -> ')
+print(1, float(1), type(1),sep=' -> ')
+print(1, str(1), type(1), sep=' -> ')
+
+
