cdded70c2838548b4c7e278238279616eabde2c9
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-01-26 20:08:55 -0300

    Input.

---
+++ Introducao_ao_python/aula015.py
@@ -0,0 +1,10 @@
+nome = input('Qual o seu nome? ')
+print(f'O seu nome é {nome=}')
+
+numero_1 = input('Dígite um número: ')
+numero_2 = input('Dígite outro número: ')
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print(f'A soma dos número é: {int_numero_1 + int_numero_2}')
