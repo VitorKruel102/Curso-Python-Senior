d75daf4a6ef4fdf08bba154a8a68288d436f42b1
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-01-26 20:17:40 -0300

    Condicionais.

---
+++ Introducao_ao_python/aula016.py
@@ -0,0 +1,9 @@
+# Condicionais:
+entrada = input('Você quer "entrar" ou "sair? ')
+
+if entrada == 'entrar':
+    print('Você entrou no sistema.')
+elif entrada == 'sair':
+    print('Você saiu do sistema.')
+else:
+    print('Você nãO digitou nem entrar e nem sair.')
