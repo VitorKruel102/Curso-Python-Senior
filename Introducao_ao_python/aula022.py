86b88c77be4e31be7cda30553287729f73fd9ffd
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-01-26 21:21:28 -0300

    Operador lógico 'or'.

---
+++ Introducao_ao_python/aula022.py
@@ -0,0 +1,10 @@
+# Operador lógico 'or'
+entrada = input('[E]Entrar [S]Sair: ')
+senha_digitada = input('Senha: ')
+
+senha_permitida = '123456'
+
+if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida:
+    print('Entrar')
+else:
+    print('Sair')
