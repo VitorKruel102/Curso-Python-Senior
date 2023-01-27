3bfe1d756d930e1686ad48119ef278b0858b2017
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-01-26 21:12:34 -0300

    Operador lógico 'and'.

---
+++ Introducao_ao_python/aula021.py
@@ -0,0 +1,10 @@
+# Operador lógico 'and'
+entrada = input('[E]Entrar [S]Sair: ')
+senha_digitada = input('Senha: ')
+
+senha_permitida = '123456'
+
+if entrada == 'E' and senha_digitada == senha_permitida:
+    print('Entrar')
+else:
+    print('Sair')
