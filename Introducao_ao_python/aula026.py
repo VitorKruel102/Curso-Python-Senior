5f55b5ae4e241bd8ce2bca2b7c1446d613ebf3ca
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-02-04 09:57:08 -0300

    Formatação de f-strings.

---
+++ Introducao_ao_python/aula026.py
@@ -0,0 +1,10 @@
+variavel = 'ABCD'
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{variavel:.<10}')
+print(f'{variavel:-^10}')
+print(f'{150.565250:.2f}')
+print(f'{15000:08X}')
+print(f'{variavel!r}') #__repr__
+print(f'{variavel!s}') #__str__
+print(f'{variavel!a}') #
