f0b533b3058bc8e491a190d4d45705d406e5bb5b
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-02-11 17:46:20 -0300

    Understanding about id function.

---
+++ Introducao_ao_python/aula031.py
@@ -0,0 +1,11 @@
+"""
+Flag (bandeira) --> Marcar um local;
+None            --> Não valor;
+is e is not     --> É ou não é;
+id              --> Ver o elemento de identificação na memória
+"""
+
+v1 = 'a'
+v2 = 'b'
+print(id(v1))
+print(id(v2))
