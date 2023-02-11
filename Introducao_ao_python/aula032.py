d8491b202ca3b1f89e5f6c856aa5af5c24088494
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-02-11 18:29:14 -0300

    Development of the resolution of the second exercise.

--- Introducao_ao_python/aula032.py
+++ Introducao_ao_python/aula032.py
@@ -15,3 +15,23 @@ else:
         print('O número é par.')
     else:
         print('O número é impar.')
+
+# Exercicio 02: 
+"""Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
+descrito, exiba a saudação apropriada. Ex:
+"""
+current_time = input('Digite a hora atual do seu dia: ')
+
+if not current_time.isdigit():
+    print('Você não digitou a hora corretamente.')
+else:
+    current_time_int = int(current_time)
+    is_morning = current_time_int <= 11 
+    is_afternoon = current_time_int >= 12 and current_time_int < 18
+
+    if is_morning:
+        print('Bom dia!')
+    elif is_afternoon:
+        print('Boa tarde!')
+    else:
+        print('Boa noite!')
