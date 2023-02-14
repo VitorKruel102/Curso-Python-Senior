981d335f2cea3166e6d7de08983d1fa5e93aea87
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-02-13 22:44:54 -0300

    Ajustando nome da variável 'contador' para 'indice'.

--- Introducao_ao_python/aula039.py
+++ Introducao_ao_python/aula039.py
@@ -4,9 +4,9 @@ nome_completo = 'Vitor Kruel da Silva'
 tamanho_nome = len(nome_completo)
 nova_string = ''
 
-contador = 0
-while contador < tamanho_nome:
-    nova_string += f'*{nome_completo[contador]}'
-    contador += 1
+indice = 0
+while indice < tamanho_nome:
+    nova_string += f'*{nome_completo[indice]}'
+    indice += 1
 
 print(nova_string)
