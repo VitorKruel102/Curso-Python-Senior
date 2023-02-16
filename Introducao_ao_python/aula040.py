f3ee1e1bf38c6b8810aeb5b5747361dffc13dbe8
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-02-16 19:07:33 -0300

    validating operators and entered numbers.

--- Introducao_ao_python/aula040.py
+++ Introducao_ao_python/aula040.py
@@ -1,7 +1,33 @@
 # Calculador com while
 
 while True:
-    pass
+    numero_1 = input('Digite um número: ')
+    numero_2 = input('Digite outro número: ')
+    operador = input('Digite o operador (+-/*): ')
+
+    numeros_validos = None
+    try:
+        numero_1_float = float(numero_1)
+        numero_2_float = float(numero_2)
+        numeros_validos = True
+    except:
+        numeros_validos = None
+
+    if numeros_validos is None:
+        print('Um ou ampos os números dígitados são inválidos.')
+        continue
+    
+    operadores_permitidos = '+-/*'
+    if operador not in operadores_permitidos:
+        print('Operador inválido')
+        continue
+
+    if len(operador) > 1:
+        print('Digite apenas um operador')
+        continue
+
+    
+
 
     sair = input('Quer sair? [s]im: ').lower().startswith('s')
     if sair:
