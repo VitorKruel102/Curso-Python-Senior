967f8d0cecef7be2a85a0e6cb8f7a25b26f9d783
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-02-23 13:56:12 -0300

    Concate in list.

--- Introducao_ao_python/aula048.py
+++ Introducao_ao_python/aula048.py
@@ -24,3 +24,10 @@ print(conjunto_numerico)
 conjunto_numerico = [10, 20, 30, 40]
 conjunto_numerico.insert(0, 5) # Adicionar um item em determinado indice
 print(conjunto_numerico)
+
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6]
+lista_c = lista_a + lista_b
+lista_a.extend(lista_b) # Adiciona a Lista_b na lista_a
+
+print(lista_c, f'{lista_a=}', sep=' --> ')
