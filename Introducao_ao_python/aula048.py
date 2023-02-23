f94e5c63e119cc521367dc767c48561ed6e16e06
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-02-23 10:40:31 -0300

    Update and Delete in list

--- Introducao_ao_python/aula048.py
+++ Introducao_ao_python/aula048.py
@@ -3,4 +3,12 @@ string = 'ABCDE'
 
 lista = ['Vitória', 123, True, 1.2, []]
 lista[-5] = 'Vitória Dias'
-print(lista)
+
+conjunto_numerico = [10, 20, 30, 40]
+conjunto_numerico[2] = 300 # Update
+
+numero = conjunto_numerico[2]
+print(conjunto_numerico, f'{numero=}', sep=' --> ')
+
+del conjunto_numerico[0] # Delete
+print(conjunto_numerico)
