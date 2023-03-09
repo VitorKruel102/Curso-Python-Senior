3bedcaef5b8f8eaad6f95efdb538bbc30da4ddce
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-03-08 21:13:03 -0300

    Tipo tuplas - 'Uma lista imutável'

---
+++ Introducao_ao_python/aula052.py
@@ -0,0 +1,8 @@
+# Tipo tuplas - 'Uma lista imutável'
+tupla_1 = ('Vitor', 'Vitoria', 'Maria')
+tupla_2 = 'Vitor', 'Vitoria', 'Maria'
+tupla_3 = tuple(['Vitor', 'Vitoria', 'Maria'])
+
+print(tupla_1)
+print(tupla_2)
+print(tupla_3)
