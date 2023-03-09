585589a97b43ff1d73b76bc6f89450bc72b68e21
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-03-08 21:02:49 -0300

    Introdução ao desempacotament + tuples (tuplas)

---
+++ Introducao_ao_python/aula051.py
@@ -0,0 +1,5 @@
+# Introdução ao desempacotament + tuples (tuplas)
+nome_1, nome_2, nome_3 = ['Vitor', 'Vitoria', 'Vania']
+nome_1, *resto = ['Vitor', 'Vitoria', 'Vania']
+nome_1, *_
+print(nome_1, _)
