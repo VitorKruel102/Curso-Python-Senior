6ec71c5d92ad8c0d8adf3d1ea3938920c5264cc7
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-03-08 21:56:13 -0300

    Exercício do carrinho de compras.

---
+++ Introducao_ao_python/aula054.py
@@ -0,0 +1,34 @@
+"""Exercicio:"""
+carrinho_de_compras = []
+
+while True:    
+    modifica_carrinho = input(
+        'Você deseka inserir[i], apagar[a],'
+        'listar[l] suas compras ou finalizar compra[f]? '
+    ).lower()
+
+    if modifica_carrinho == 'i':
+        produto = input('Informe o nome do produto: ').lower()
+        carrinho_de_compras.append(produto)
+        print(f'Este é a suas lista de compras: {carrinho_de_compras}')
+    elif modifica_carrinho == 'a':
+        print(f'Este é a suas lista de compras: {carrinho_de_compras}')
+        produto = input('Informe o nome do produto que deseja apagar: ').lower()
+
+        if not produto in carrinho_de_compras:
+            print('Não temos esse produto na lista, tente novamente mais tarde.')
+            continue
+
+        for index, item in enumerate(carrinho_de_compras):
+            if item == produto:
+                carrinho_de_compras.pop(index)
+    elif modifica_carrinho == 'l':
+        print(f'Este é a suas lista de compras: {carrinho_de_compras}')
+    elif modifica_carrinho == 'f':
+        print(f'Este é a suas lista de compras: {carrinho_de_compras}')
+        print('Suas compras estão finalizadas. Obrigado pela preferência.')
+        break
+    else:
+        print('Dígite uma solicitação valida!')
+    
+    
