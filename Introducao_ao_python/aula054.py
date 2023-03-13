61e107e629c847d78d8f5241b80f1b4b2269640c
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-03-13 10:50:00 -0300

    Update.

--- Introducao_ao_python/aula054.py
+++ Introducao_ao_python/aula054.py
@@ -10,23 +10,33 @@ while True:
     if modifica_carrinho == 'i':
         produto = input('Informe o nome do produto: ').lower()
         carrinho_de_compras.append(produto)
-        print(f'Este é a suas lista de compras: {carrinho_de_compras}')
+        print(
+            f'Este é a suas lista de compras: {carrinho_de_compras}'
+        )
     elif modifica_carrinho == 'a':
-        print(f'Este é a suas lista de compras: {carrinho_de_compras}')
+        print(
+            f'Este é a suas lista de compras: {carrinho_de_compras}'
+        )
         produto = input('Informe o nome do produto que deseja apagar: ').lower()
 
         if not produto in carrinho_de_compras:
-            print('Não temos esse produto na lista, tente novamente mais tarde.')
+            print(
+                'Não temos esse produto na lista, tente novamente mais tarde.'
+            )
             continue
 
         for index, item in enumerate(carrinho_de_compras):
             if item == produto:
                 carrinho_de_compras.pop(index)
     elif modifica_carrinho == 'l':
-        print(f'Este é a suas lista de compras: {carrinho_de_compras}')
+        print(
+            f'Este é a suas lista de compras: {carrinho_de_compras}'
+        )
     elif modifica_carrinho == 'f':
-        print(f'Este é a suas lista de compras: {carrinho_de_compras}')
-        print('Suas compras estão finalizadas. Obrigado pela preferência.')
+        print(
+            f'Este é a suas lista de compras: {carrinho_de_compras}\n'
+            'Suas compras estão finalizadas. Obrigado pela preferência.'
+        )
         break
     else:
         print('Dígite uma solicitação valida!')
