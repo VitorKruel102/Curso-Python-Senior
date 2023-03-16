a38e40854f105ce4a32cfadcc14d20ec810434c0
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-03-16 13:11:55 -0300

    Escopo de funções em Python.

---
+++ Introducao_ao_python/Seção_4/aula068.py
@@ -0,0 +1,22 @@
+# Escopo de funções em Python
+
+x = 1
+
+
+def escopo():
+    """."""
+    global x
+    x = 10
+
+    def outra_funcao():
+        """."""
+        x = 11
+        y = 2
+        print(x + y)
+    
+    outra_funcao()
+    print(x)
+
+
+print(x)
+escopo()
