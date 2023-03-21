d7d20db7e29000600ce6e0a3bf794f2774659b86
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-03-21 11:49:24 -0300

    Entendendo sobre Dicionário

---
+++ Introducao_ao_python/Seção_4/aula076.py
@@ -0,0 +1,16 @@
+# Dicionário:
+perfil_vitor = {
+    'nome': 'Vitor',
+    'sobrenome': 'Kruel',
+    'idade': 22,
+    'altura': 1.71,
+    'endereços': [
+        {'rua': 'Rua Field', 'Número': 523},
+        {'rua': 'Rua Felipe Shil', 'Número': 862},
+    ]
+}
+
+print(perfil_vitor['nome'])
+print(perfil_vitor['sobrenome'])
+print(perfil_vitor['endereços'][0]['rua'])
+print(perfil_vitor['endereços'][1]['rua'])
