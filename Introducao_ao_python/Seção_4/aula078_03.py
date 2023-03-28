ca898ab28ed76ba3562807e1edfcf9dc4a37daf2
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-03-28 11:57:32 -0300

    Adicionado exemplo de diferença simétrica

--- Introducao_ao_python/Seção_4/aula078_03.py
+++ Introducao_ao_python/Seção_4/aula078_03.py
@@ -4,3 +4,4 @@ conjunto2 = set([6, 7, 8, 9, 10])
 uniao_set = conjunto1 | conjunto2
 intersecao = conjunto1 & conjunto2
 diferenca = conjunto1 - conjunto2
+diferenca_simetrica = conjunto1 ^ conjunto2 # Itens que não estão em ambos
