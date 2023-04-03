731fd1fe08679f3d6176c8cae00d2e41bc40b819
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-04-03 20:32:43 -0300

    Mapeamento de dados em List Comprehension:

--- Introducao_ao_python/Seção_4/aula084.py
+++ Introducao_ao_python/Seção_4/aula084.py
@@ -7,3 +7,16 @@ print(lista)
 # List Comprehension:
 lista = [x for x in range(10)]
 print(lista)
+
+# Mapeamento de dados em List Comprehension:
+lista = [numero * 2 for numero in lista]
+print(lista)
+
+produtos = [
+    {'nome': 'p1', 'preco': 10,},
+    {'nome': 'p2', 'preco': 20,},
+    {'nome': 'p3', 'preco': 30,},
+]
+
+novos_produtos = [ {**produto, 'preco': produto['preco'] * 1.05} for produto in produtos]
+print(novos_produtos)
