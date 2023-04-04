e85092589d4883edc374dec060188485068bc6e3
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-04-04 11:12:12 -0300

    Gerator expression, Iterables, Iterators:

--- Introducao_ao_python/Seção_4/aula090.py
+++ Introducao_ao_python/Seção_4/aula090.py
@@ -1,3 +1,4 @@
+import sys
 # Gerator expression, Iterables, Iterators:
 iterable = ['Eu', 'tenho', '__iter__']
 itarator = iter(iterable) # Permite utilizar o next()
@@ -5,3 +6,11 @@ itarator = iter(iterable) # Permite utilizar o next()
 print(next(itarator))
 print(next(itarator))
 print(next(itarator))
+
+
+generator = (n for n in range(100000000000000)) 
+"""
+Genetator Expression, entraga um valor por vez,
+não pesa na memória.
+"""
+print(sys.getsizeof(generator))
