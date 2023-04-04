9d6ad8f55b2054502685579dff9437d738ff9207
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-04-04 11:04:40 -0300

    Gerator expression, Iterables, Iterators

---
+++ Introducao_ao_python/Seção_4/aula090.py
@@ -0,0 +1,7 @@
+# Gerator expression, Iterables, Iterators:
+iterable = ['Eu', 'tenho', '__iter__']
+itarator = iter(iterable) # Permite utilizar o next()
+
+print(next(itarator))
+print(next(itarator))
+print(next(itarator))
