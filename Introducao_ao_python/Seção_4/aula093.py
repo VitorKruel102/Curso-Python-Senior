7584fa78d09cc4df736c4fc67274de200898dc7b
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-04-10 10:52:05 -0300

    Part01 - Try, except, else, finally.

---
+++ Introducao_ao_python/Seção_4/aula093.py
@@ -0,0 +1,10 @@
+# Try, except, else, finally
+try:
+    a = 18
+    b = 0
+    c = a / b
+except ZeroDivisionError:
+    print('Dividiu por zero.')
+except NameError:
+    print('Nome não está definido')
+print('FINALIZOU')
