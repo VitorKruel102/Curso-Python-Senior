661594aa859de87a832936df37e5ba7b922846d3
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-04-11 10:46:27 -0300

    Entendendo sobre módulos.

---
+++ Introducao_ao_python/Seção_4/aula096.py
@@ -0,0 +1,10 @@
+import sys
+
+
+print(sys.platform) # Qual o sistema operacional
+
+from sys import exit, platform
+
+print(platform) # Qual o sistema operacional
+print(exit())   # Fecha o programa
+
