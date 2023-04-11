f4df803c1a78849619d010f4f1a5ef91698536d8
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-04-11 11:07:53 -0300

    Entendendo sobre __all__

--- Introducao_ao_python/Seção_4/aula99_package/modulo.py
+++ Introducao_ao_python/Seção_4/aula99_package/modulo.py
@@ -1,2 +1,13 @@
+__all__ = [
+    'variavel',
+    'soma',
+] 
+"""Quando chamado todo o modulo (*), só o que tiver dentro
+do __all__ será permitido o acesso.
+"""
+
+
+variavel = 'Nome'
+
 def soma(x, y):
     return x + y
