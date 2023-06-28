92565c53e1c8199678b550688f792e150eea327b
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-06-28 09:53:59 -0300

    Apagando, renomeando e movendo arquivos.

---
+++ Introducao_ao_python/Seção_04/aula_189.py
@@ -0,0 +1,14 @@
+"""
+Apagando, renomeando e movendo arquivos
+
+Criado: 28/06/2023 09:49
+
+Autor: Vitor Kruel
+"""
+import os
+
+# Remover arquivo
+os.remove('<path>')
+
+# Renomeando arquivo ou Movendo arquivo
+os.rename('<path_antigo>', '<path_novo>')
---
+++ Introducao_ao_python/Seção_04/aula_190.py
@@ -0,0 +1,7 @@
+"""
+Salvando dados Python em Json
+
+Criado: 28/06/2023 09:49
+
+Autor: Vitor Kruel
+"""
