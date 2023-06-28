dd23d7157c51fca4d385bef26e065a04fc00f914
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-06-28 15:17:47 -0300

    Salvando e lendo dados Python em Json.

--- Introducao_ao_python/Seção_04/aula_190.py
+++ Introducao_ao_python/Seção_04/aula_190.py
@@ -1,7 +1,59 @@
 """
-Salvando dados Python em Json
+Salvando e lendo dados Python em Json
 
 Criado: 28/06/2023 09:49
 
 Autor: Vitor Kruel
 """
+import json
+import os
+
+
+from pprint import pprint
+
+
+BASE_DIR = os.path.dirname(__file__)
+SAVE_TO = os.path.join(BASE_DIR, 'arquivo-python.json')
+
+pessoas = [
+    {
+        "Nome:": "Vitor",
+        "Sobrenome": "Kruel da Silva",
+        "Estado": "RS",
+        "Cidade": "São Leopoldo",
+        "Idade": 22,
+        "Telefone": {
+            "Residencial": "00 0000-0000",
+            "Pessoal": "00 0 0000-0000"
+        }
+    },
+    {
+        "Nome:": "Vitória Carolina",
+        "Sobrenome": "Dias da Silva",
+        "Estado": "RS",
+        "Cidade": "Guaíba",
+        "Idade": 22,
+        "Telefone": {
+            "Residencial": "00 0000-0000",
+            "Pessoal": "00 0 0000-0000"
+        }
+    },
+    {
+        "Nome:": "Vânia",
+        "Sobrenome": "Renner Kruel",
+        "Estado": "RS",
+        "Cidade": "São Leopoldo",
+        "Idade": 49,
+        "Telefone": {
+            "Residencial": "00 0000-0000",
+            "Pessoal": "00 0 0000-0000"
+        }
+    }
+]
+
+with open(SAVE_TO, 'w', encoding='utf-8') as arquivo:
+    json.dump(pessoas, arquivo, indent=2, ensure_ascii=False)
+
+with open(SAVE_TO, 'r', encoding='utf-8') as arquivo:
+    pessoas = json.load(arquivo)
+    pprint(pessoas)
