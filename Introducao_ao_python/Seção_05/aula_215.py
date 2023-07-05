ad31b6817ec86ef48ee4a3d906bcc66f280c048a
Vitor Kruel <vkruel.programador@gmail.com>
2023-07-05 19:47:27 -0300

    Relações entre classes: Associação, agregação e composição.

---
+++ Introducao_ao_python/Seção_05/aula_215.py
@@ -0,0 +1,40 @@
+"""
+Relações entre classes: Associação, agregação e composição.
+
+Criado: 05/07/2023 19:30
+
+Author: Vitor Kruel.
+"""
+
+
+class Escritor:
+    def __init__(self, nome) -> None:
+        self.nome = nome
+        self._ferramenta = None
+
+    @property
+    def ferramenta(self):
+        return self._ferramenta
+
+    @ferramenta.setter
+    def ferramenta(self, ferramenta):
+        self._ferramenta = ferramenta
+
+
+class FerramentaDeEscrever:
+    def __init__(self, nome) -> None:
+        self.nome = nome
+
+    def escrever(self):
+        return f'{self.nome} está escrevendo'
+
+
+escritor = Escritor('Luiz')
+caneta = FerramentaDeEscrever('Caneta Bic')
+maquina_de_escrever = FerramentaDeEscrever('Máquina')
+
+escritor.ferramenta = maquina_de_escrever
+
+print(caneta.escrever())
+print(maquina_de_escrever.escrever())
+print(escritor.ferramenta.escrever())
