ff03155269dbb36e42657287559923b99a0d4620
Vitor Kruel <vkruel.programador@gmail.com>
2023-07-14 10:41:28 -0300

    Adicionando subclasse para a abstração.

--- Introducao_ao_python/Seção_05/aula225/log.py
+++ Introducao_ao_python/Seção_05/aula225/log.py
@@ -1,16 +1,34 @@
 # Abstração 
 class Log:
-    def log(self, mensagem):
+    def _log(self, mensagem):
         """."""
         raise NotImplementedError('Implemente o método log')
+    
+    def log_error(self, mensagem):
+        """."""
+        return self._log(f'Error: {mensagem}')
+    
+    def log_success(self, mensagem):
+        """."""
+        return self._log(f'Success: {mensagem}')
 
 
 class LogFileMixin(Log):
-    def log(self, mensagem):
+    def _log(self, mensagem):
         """."""
         print(mensagem)
 
 
+class LogPrintMixin(Log):
+     def _log(self, mensagem):
+        """."""
+        print(f'{mensagem} ({self.__class__.__name__})')
+
+ 
 if __name__ == '__main__':
     objeto = LogFileMixin()
-    objeto.log('qualquer coisa')
+    objeto.log_error('mensagem')
+
+    objeto = LogPrintMixin()
+    objeto.log_error('qualquer coisa')
+    objeto.log_success('qualquer coisa')
